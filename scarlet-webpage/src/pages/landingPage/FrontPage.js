8e129c4704548b2236665dca84d543a7cec759b3
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-09 18:38:27 -0400

    created file for the front page

---
+++ scarlet-webpage/src/pages/landingPage/FrontPage.js
@@ -0,0 +1,17 @@
+import Top from './Top'
+import Center from './Center'
+import Footer from './Footer';
+
+
+const FrontPage = () => {
+
+    return (
+        <div>
+            <Top />
+            <Center />
+            <Footer />
+        </div>
+    )
+}
+
+export default FrontPage;
