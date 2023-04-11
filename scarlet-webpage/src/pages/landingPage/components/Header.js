f7e2825be09bb07c94a2501a345030c19a9851cf
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-11 18:01:46 -0400

    moved styles to css file and added app icon to the header. also made navigation into separate component

--- scarlet-webpage/src/pages/landingPage/components/Header.js
+++ scarlet-webpage/src/pages/landingPage/components/Header.js
@@ -1,38 +1,17 @@
-import { Button, Box, List, ListItemText, ListItem, ListItemButton, Typography } from '@mui/material';
-import { Link as RouterLink} from 'react-router-dom';
-const Header = () => {
+//My Files
+import NavList from './NavList'
+import "./../../../styles/landingPageStyles/index.css"
 
-    const Item = ({ text, to, selected }) => {
-        return (
-            <ListItem key={text}
-                selected={selected === text}
-                disablePadding sx={{borderRadius: "5px",
-                    "&.MuiListItem-root": {
-                        ":hover": {
-                            backgroundColor: "rgba(0,0,0,0.5)",
-                            color: "white",
-                            transition: "background-color 500ms linear"
-                        }
-                    }}}>
-                <ListItemButton component={RouterLink} to={to}>
-                    <ListItemText sx={{whiteSpace: "nowrap"}}>
-                        {text}
-                    </ListItemText>
-                </ListItemButton>
-            </ListItem>)
-    }
+//Material UI
+import { Box } from '@mui/material'
+
+const Header = () => {
 
     return (
         <Box className="header">
-            <Box className="header-wrapper" sx={{display: "flex", justifyContent: "right"}}>
-                <nav>
-                    <List sx={{display: "flex"}}>
-                        <Item text={"Home"} to={"/"}/>
-                        <Item text={"Stories"} to={"/stories"}/>
-                        <Item text={"About Scarlet"} to={"/about-scarlet"}/>
-                        <Item text={"Contact Us"} to={"/contact-us"}/>
-                    </List>
-                </nav>
+            <Box className="header-wrapper">
+                <img src={"/appstore.png"} alt={"Periodic App Icon"}/>
+                <NavList/>
             </Box>
         </Box>
     )
