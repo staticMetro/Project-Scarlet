0a81bd32225f7eaa76fb89ef860cb340984f73da
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-11 18:29:52 -0400

    added link to terms, privacy and copyright

--- scarlet-webpage/src/pages/landingPage/components/Footer.js
+++ scarlet-webpage/src/pages/landingPage/components/Footer.js
@@ -5,22 +5,31 @@ import NavList from './NavList'
 //Material UI
 import { Box } from '@mui/material';
 
+//React Files
+import { Link as RouterLink } from 'react-router-dom'
+
 function Footer() {
     return (
         <Box className="footer">
             <Box className="footer-wrapper">
                 <NavList />
-                <Box sx={{whiteSpace: "nowrap"}}>
-                    <Box component="span">
-                        {`Terms and Conditions | `}
-                    </Box>
-                    <Box component="span">
-                        {`Privacy Policy | `}
-                    </Box>
-                    <Box component="span">
-                        All Rights Reserved &copy;2023 Periodic App
-                    </Box>
-                    </Box>
+                <Box sx={{ whiteSpace: "nowrap" }}>
+                    <RouterLink to={"/TermsAndConditions"}>
+                        <Box component="span">
+                            {`Terms and Conditions | `}
+                        </Box>
+                    </RouterLink>
+                    <RouterLink to={"/PrivacyPolicy"}>
+                        <Box component="span">
+                            {`Privacy Policy | `}
+                        </Box>
+                    </RouterLink>
+                    <RouterLink to={"/Copyright"}>
+                        <Box component="span">
+                            All Rights Reserved &copy;2023 Periodic App
+                        </Box>
+                    </RouterLink>
+                </Box>
             </Box>
         </Box>
     )
