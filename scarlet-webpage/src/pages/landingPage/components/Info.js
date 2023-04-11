cef5b28ad9dd71c04ef2ec3a312884f951a6dfe6
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-11 18:02:06 -0400

    Made section for information about the application

---
+++ scarlet-webpage/src/pages/landingPage/components/Info.js
@@ -0,0 +1,16 @@
+//My Files
+import "./../../../styles/landingPageStyles/index.css"
+
+//Material UI
+import { Box } from '@mui/material';
+
+const Info = () => {
+
+    return (
+        <Box className="info">
+           Info 
+        </Box>
+    )
+}
+
+export default Info;
