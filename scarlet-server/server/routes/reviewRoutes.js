60fc336814a985e082b27c71f54d548b47883294
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-18 23:20:46 -0400

    was not importing the reviewController. fixed

--- scarlet-server/server/routes/reviewRoutes.js
+++ scarlet-server/server/routes/reviewRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userController = require('./../controllers/userController')
+const reviewController = require('./../controllers/reviewController')
 
 const router = express.Router();
 
