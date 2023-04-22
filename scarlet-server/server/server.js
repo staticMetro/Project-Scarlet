7a1f47474b13ca12cebeb9a8ea47187d4cbfd760
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-21 23:39:37 -0400

    changed dontenv config because morgan was not working

--- scarlet-server/server/server.js
+++ scarlet-server/server/server.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' })
 const app = require('./app');
 
-dotenv.config({ path: './config.env' })
+
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
     .replace('<username>', process.env.DATABASE_USERNAME)
