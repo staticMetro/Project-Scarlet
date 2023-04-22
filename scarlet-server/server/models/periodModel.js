434ba187be99d7b7dc769e48ac294eefc76502d8
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-21 20:15:12 -0400

    fixed sexuallyActive validator not working

--- scarlet-server/server/models/periodModel.js
+++ scarlet-server/server/models/periodModel.js
@@ -3,31 +3,39 @@ const mongoose = require('mongoose');
 const periodSchema = new mongoose.Schema({
     periodStart: {
         type: Date,
-        default: Date.now()
+        default: Date.now
     },
     periodEnd: {
         type: Date,
+        default: Date.now
     },
     nextPeriod: {
         type: Date,
+        default: Date.now
     },
     fertileWindowStart: {
         type: Date,
+        default: Date.now
     },
     fertileWindowEnd: {
-        type: Date
+        type: Date,
+        default: Date.now
     },
     futurePeriods: Array,
     sexuallyActive: {
         type: Boolean,
-        required: [true, "You must input whether you are sexually active"]
+        required: [true, "You must input your sexual activity"]
     },
     flow: {
         type: String,
-        enum: ["heavy", "medium", "low"]
+        enum: ["heavy", "medium", "light"]
     },
     chanceOfPregnancy: {
         type: Number,
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
     }
 })
 
