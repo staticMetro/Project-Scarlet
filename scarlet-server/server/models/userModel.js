35c3c28bb2c4b4af6465b81473d0d7be337866e6
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-24 18:43:27 -0400

    added some validators on the names

--- scarlet-server/server/models/userModel.js
+++ scarlet-server/server/models/userModel.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: [true, "You must input your first name"]
+        required: [true, "You must input your first name"],
+        maxLength: [20, "First name must have less than 20 characters"]
     },
     lastName: {
         type: String,
-        required: [true, "You must input your last name"]
+        required: [true, "You must input your last name"],
+        maxLength: [20, "Last name must have less than 20 characters"]
     },
     email: {
         type: String,
