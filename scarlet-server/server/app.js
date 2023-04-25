5eae4b65884536463ea7979a21837a33b91b3aa5
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-25 17:52:44 -0400

    added error handling to all false app routes using global error handler

--- scarlet-server/server/app.js
+++ scarlet-server/server/app.js
@@ -3,6 +3,7 @@ const compression = require('compression')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const globalErrorHandler = require('./controllers/errorController')
 const AppError = require('./utils/appError')
 const userRouter = require("./routes/userRoutes")
 const periodRouter = require("./routes/periodRoutes");
@@ -36,13 +37,5 @@ app.all("*", function (request, response, next) {
     next(new AppError(`Can not find ${request.originalUrl} on this server`))
 })
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode ?? 500;
-    err.status = err.status ?? 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-})
+app.use(globalErrorHandler)
 module.exports = app;
