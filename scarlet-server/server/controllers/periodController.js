56e94957688a832f1954a3a8c98bbe37908072c6
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-25 17:55:36 -0400

    added error handling for periods not found

--- scarlet-server/server/controllers/periodController.js
+++ scarlet-server/server/controllers/periodController.js
@@ -1,66 +1,79 @@
 const Period = require('./../models/periodModel')
 const APIFeatures = require('./../utils/apiFeatures')
 const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
 
 exports.getAllPeriods = catchAsync(async (request, response, next) => {
-        const features = new APIFeatures(Period.find(), request.query).filter().paginate().limitFields().sort();
-        const periods = await features.query; 
+    const features = new APIFeatures(Period.find(), request.query).filter().paginate().limitFields().sort();
+    const periods = await features.query;
 
-        response.status(200).json({
-            status: "success",
-            results: periods.length,
-            data: {
-                periods
-            }
-        })
+    response.status(200).json({
+        status: "success",
+        results: periods.length,
+        data: {
+            periods
+        }
+    })
 })
 
 exports.getPeriod = catchAsync(async (request, response, next) => {
-        const period = await Period.findById(request.params.id);
+    const period = await Period.findById(request.params.id);
 
-        response.status(200).json({
-            status: "success",
-            data: {
-                period
-            }
-        })
+    if (!period) {
+        return next(new AppError('No tour found with that Id'))
+    }
+    response.status(200).json({
+        status: "success",
+        data: {
+            period
+        }
+    })
 })
 
 exports.updatePeriod = catchAsync(async (request, response, next) => {
 
-        const period = await Period.findByIdAndUpdate(request.params.id, request.body, {
-            new: true,
-            runValidators: true});
+    const period = await Period.findByIdAndUpdate(request.params.id, request.body, {
+        new: true,
+        runValidators: true
+    });
 
-        response.status(200).json({
-            status: "success",
-            data: {
-                period
-            }
-        })
-    
+    if (!period) {
+        return next(new AppError('No tour found with that Id'))
+    }
+
+    response.status(200).json({
+        status: "success",
+        data: {
+            period
+        }
+    })
+
 })
 
 exports.deletePeriod = catchAsync(async (request, response, next) => {
-   
-        await Period.findByIdAndDelete(request.params.id);
 
-        response.status(200).json({
-            status: "success",
-            data: null
-        })
+    const period = await Period.findByIdAndDelete(request.params.id);
+
+    if (!period) {
+        return next(new AppError('No tour found with that Id'))
+    }
+
+    response.status(200).json({
+        status: "success",
+        data: null
+    })
 
 })
 
 exports.createPeriod = catchAsync(async (request, response, next) => {
 
-        const newPeriod = await Period.create(request.body);
+    const newPeriod = await Period.create(request.body);
 
-        response.status(201).json({
-            status: "success",
-            data: {
-                period: newPeriod
-            }
-        })
-    
+    response.status(201).json({
+        status: "success",
+        data: {
+            period: newPeriod
+        }
+    })
+
 })
