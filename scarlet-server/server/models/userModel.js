37b91bee480be83dd1574db47b62eb3b62098917
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-27 19:52:12 -0400

    update model with validators and instance methods for passwords

--- scarlet-server/server/models/userModel.js
+++ scarlet-server/server/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs')
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -13,17 +15,48 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "You must input your email"],
-        unique: true
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide an email"] 
     },
     password: {
         type: String,
-        required: [true, "You must input your password"]
+        required: [true, "You must input your password"],
+        minLength: [8, "Password must have at least 8 characters"],
+        select: false
     },
-    confirmPassword: {
+    //Only works on save;
+    passwordConfirm: {
         type: String,
+        required: [true, "Please confirm your password"],
+        validate: {
+            validator: function(el) {
+                return this.password === el;
+            }
+            ,
+            message: 'Passwords are not the same!'
+        }
     },
-    periods: {}
+    
 })
+
+userSchema.pre('save', async function (next) {
+
+    if (!this.isModified('password')) {
+        return next();
+    } else {
+        //Encrypts password
+        this.password = await bcrypt.hash(this.password, 12);
+        //removes password confirm field
+        this.passwordConfirm = undefined;
+        next()
+    }
+
+})
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword)
+}
 
 const User = mongoose.model("User", userSchema);
 
