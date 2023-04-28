403d3368484d2006bf93f8c120d6bbc220a514a8
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-04-28 02:57:12 -0400

    added restriction based on roles

--- scarlet-server/server/controllers/authController.js
+++ scarlet-server/server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('./../models/userModel')
 const catchAsync = require('./../utils/catchAsync')
+const { promisify } = require('util')
 const jwt = require('jsonwebtoken')
 const AppError = require('./../utils/appError')
 
@@ -52,4 +53,46 @@ exports.login = catchAsync(async (request, response, next) => {
         status: "success",
         token
     })
+})
+
+
+
+exports.protect = catchAsync(async (request, response, next) => {
+    //Get Token
+    let token;
+    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer')) {
+        token = request.headers.authorization.split(' ')[1];
+    }
+    
+    if (!token) {
+        return next(new AppError('You are not logged in', 401));
+    }
+
+    //Verify token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    // Check if user still exists
+    const freshUser = await User.findById(decoded.id)
+    if (!freshUser) {
+        return next(new AppError("The user belonging to the token no longer exists", 401));
+    }
+
+    //Check if user changed password after the token was issued
+    if (freshUser.changedPasswordAfter(decoded.iat)) {
+        return next(new AppError ("User recently changed password! Please log in again.", 401))
+    }
+    
+    request.user = freshUser;
+    next();
+})
+
+exports.restrictTo = catchAsync(async (request, response, next) => {
+
+    return (...roles) => {
+        for (const role of roles) {
+            if (role === request.user.role) {
+                return next();
+            }
+        }
+        return next(new AppError("You are not allowed to access this route", 401))
+    }
 })
