20f26c10e2c43760718d13835f161a3c290642d7
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-05-05 22:44:11 -0400

    added route for updating yourself when logged in adn filtered the request body for security

--- scarlet-server/server/controllers/userController.js
+++ scarlet-server/server/controllers/userController.js
@@ -1,5 +1,18 @@
 const User = require('./../models/userModel')
 const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError');
+
+const filteredObj = (obj, ...allowedFields) => {
+
+    Object.keys(obj).forEach((element, i, array) => {
+        if (!allowedFields.includes(element)) {
+            delete obj[element]
+        }
+
+    })
+    return obj;
+}
+
 exports.getAllUsers = catchAsync(async (request, response, next) => {
 
     const users = await User.find();
@@ -10,8 +23,26 @@ exports.getAllUsers = catchAsync(async (request, response, next) => {
             users
         }
     })
+})
 
+exports.updateMe = catchAsync(async (request, response, next) => {
+    console.log('here')
+    if (request.body.password || request.body.passwordConfirm) {
+        return next(new AppError('This route is not for updating your password', 400));
+    }
 
+    const filteredBody = filteredObj(request.body, 'name', 'email');
+    const user = await User.findByIdAndUpdate(request.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    })
+
+    response.status(200).json({
+        status: 'success',
+        data: {
+            user
+        }
+    })
 })
 
 exports.getUser = (request, response) => {
