6ff5726833e35afffd36f522849eef88eb7bdb61
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-05-05 20:43:30 -0400

    added route for updating password when already logged in

--- scarlet-server/server/routes/userRoutes.js
+++ scarlet-server/server/routes/userRoutes.js
@@ -7,12 +7,13 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login)
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:resetToken', authController.resetPassword)
+router.patch('/updatePassword', authController.protect, authController.updatePassword);
 
 router
     .route('/')
     .get(userController.getAllUsers)
     .post(userController.createUser)
-
+    
 router
     .route('/:id')
     .get(userController.getUser)
