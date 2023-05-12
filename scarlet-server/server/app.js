0c2e2913a7b99d36a5f9ffa7944431742efcfde9
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-05-11 21:07:19 -0400

    implemented packages for security

--- scarlet-server/server/app.js
+++ scarlet-server/server/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const compression = require('compression')
 const cors = require('cors')
 const morgan = require('morgan')
+const rateLimit = require("express-rate-limit")
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
+const hpp = require('hpp');
 
 const globalErrorHandler = require('./controllers/errorController')
 const AppError = require('./utils/appError')
@@ -11,12 +16,38 @@ const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
 
+//HTTP SECURITY HEADERS
+app.use(helmet());
+
+//LIMIT REQUEST FROM THE SAME API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
 
 app.use(compression());
 app.use(cors());
 
 //Body Parser
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+
+//DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+//DATA SANITIZATION 
+app.use(xss());
+
+//PREVENT PARAMETER POLLUTION;
+app.use(hpp({
+    whitelist: {
+        duration: [flow, sexuallyActive, periodStart]
+    }
+}));
+//SERVING STATIC FILES
+app.use(express.static(`${__dirname}/public`));
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
