8f305881dba69789918d9512eb979864dd9a5b6d
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-05-11 21:06:16 -0400

    added query middleware to only query for active users

--- scarlet-server/server/models/userModel.js
+++ scarlet-server/server/models/userModel.js
@@ -109,6 +109,11 @@ userSchema.methods.createPasswordResetToken = function () {
     return resetToken;
 }
 
+userSchema.pre(/^find/, function (next) {
+    this.find({ active: {$ne: false} });
+    next();
+})
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
